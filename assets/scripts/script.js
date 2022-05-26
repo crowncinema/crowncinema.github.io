b92ccf293c584733064dea3259294a49b92d94a5
zeke-taylor <etaylor0234@gmail.com>
2022-05-26 12:15:08 -0500

    Update script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,7 @@ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
+  let i;
   var slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
